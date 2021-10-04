1ac021cc21efc8a27268a323997fe09fb16337d7
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-04 14:44:56 +0800

    Initial Commit

---
+++ Instagrid/InstagridApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagridApp.swift
+//  Instagrid
+//
+//  Created by Giovanni Gaffé on 2021/10/4.
+//
+
+import SwiftUI
+
+@main
+struct InstagridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
