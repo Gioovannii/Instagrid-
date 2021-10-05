6f84991a5e176155a5a5421bde84aff01b83fdce
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-05 14:51:27 +0800

    Grid  view

---
+++ Instagrid/GridLayoutView.swift
@@ -0,0 +1,82 @@
+//
+//  GridLayoutView.swift
+//  Instagrid
+//
+//  Created by Giovanni Gaffé on 2021/10/5.
+//
+
+import SwiftUI
+
+struct GridLayoutView: View {
+    @State private var hideButtonTL = false
+    @State private var hideButtonTR = false
+    @State private var hideButtonBL = false
+    @State private var hideButtonBR = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: { print("Top Left tapped")
+                    hideButtonTL.toggle()
+                }) {
+                    HStack(spacing: 10) {
+                        Image(uiImage: Constant.imagePicture)
+                            .resizable()
+                            .frame(width: 120, height: 120, alignment: .center)
+                    }
+                    .opacity(hideButtonTL ? 0 : 1)
+                }
+                
+                
+                
+                Button(action: { print("Top Right tapped")
+                    hideButtonTR.toggle()
+                }) {
+                    HStack(spacing: 10) {
+                        Image(uiImage: Constant.imagePicture)
+                            .resizable()
+                            .frame(width: 120, height: 120, alignment: .center)
+                    }
+                    .opacity(hideButtonTR ? 0 : 1)
+                }
+            }
+            
+            
+            HStack {
+                Button(action: { print("Bottom left tapped")
+                    hideButtonBL.toggle()
+                }) {
+                    HStack(spacing: 10) {
+                        Image(uiImage: Constant.imagePicture)
+                            .resizable()
+                            .frame(width: 120, height: 120, alignment: .center)
+                    }
+                    .opacity(hideButtonBL ? 0 : 1)
+                }
+                
+                Button(action: { print("Bottom right tapped")
+                    hideButtonBR.toggle()
+                }) {
+                    HStack(spacing: 10) {
+                        Image(uiImage: Constant.imagePicture)
+                            .resizable()
+                            .frame(width: 120, height: 120, alignment: .center)
+                    }
+                    .opacity(hideButtonBR ? 0 : 1)
+                }
+            }
+            
+        }
+        .padding()
+        .background(Color.green)
+        .border(Color.white, width: 10)
+        .cornerRadius(20)
+        .padding(50)
+    }
+}
+
+struct GridLayoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridLayoutView()
+    }
+}
