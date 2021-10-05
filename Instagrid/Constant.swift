5ee516f9c43e689b55b29e406a8d8230e4aa668e
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-05 14:51:55 +0800

    Constant view

---
+++ Instagrid/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  Instagrid
+//
+//  Created by Giovanni Gaffé on 2021/10/4.
+//
+
+import SwiftUI
+
+struct Constant {
+    static let imagePicture = UIImage(named: "grande")!
+    static let layout1 = UIImage(named: "Layout 1")!
+    static let layout2 = UIImage(named: "Layout 2")!
+    static let layout3 = UIImage(named: "Layout 3")!
+
+    struct Config {
+        // TODO: - Here we can do so key privacy
+
+    }
+    
+    struct Color {
+       
+    }
+}
