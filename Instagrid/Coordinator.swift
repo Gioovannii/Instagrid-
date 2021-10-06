438b82cee9da8448b39607b1e8ee27b018f32144
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-06 16:40:09 +0800

    add coordinator and image picker

---
+++ Instagrid/Coordinator.swift
@@ -0,0 +1,23 @@
+//
+//  Coordinator.swift
+//  Instagrid
+//
+//  Created by Giovanni Gaffé on 2021/10/6.
+//
+
+import UIKit
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
