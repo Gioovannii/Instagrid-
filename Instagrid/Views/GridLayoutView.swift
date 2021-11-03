1b94cc66c41b894c1961ddb568d88a2fddfc7549
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-03 16:31:46 +0800

    Refactor project

--- Instagrid/Views/GridLayoutView.swift
+++ Instagrid/Views/GridLayoutView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct GridLayoutView: View {
-    
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
-    @State private var selectedImage: [UIImage?]
     @State private var isImagePickerDisplay = false
+    @State private var selectedImage: UIImage?
+    
+    
     
     var body: some View {
+        //        Image(uiImage: selectedImage ?? Constant.imagePicture)
+        
         VStack {
             HStack {
                 Button(action: { print("Top Left tapped")
                     self.sourceType = .camera
                     self.isImagePickerDisplay.toggle()
-                })
-                    {
+                }) {
                     HStack(spacing: 10) {
                         if selectedImage != nil {
-                            Image(uiImage: selectedImage[0] ?? UIImage())
+                            Image(uiImage: selectedImage ?? UIImage())
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
                         } else {
@@ -33,11 +35,6 @@ struct GridLayoutView: View {
                         }
                     }
                 }
-                .sheet(isPresented: $isImagePickerDisplay) {
-                    ImagePickerView(selectedImage: $selectedImage[0], sourceType: .camera)
-                    ImagePickerView(selectedImage: $selectedImage[0], sourceType: .photoLibrary)
-                }
-                
                 
                 Button(action: { print("Top Right tapped")
                     self.sourceType = .camera
@@ -45,7 +42,7 @@ struct GridLayoutView: View {
                 }) {
                     HStack(spacing: 10) {
                         if selectedImage != nil {
-                            Image(uiImage: selectedImage[1] ?? UIImage())
+                            Image(uiImage: selectedImage ?? UIImage())
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
                         } else {
@@ -55,22 +52,24 @@ struct GridLayoutView: View {
                         }
                     }
                 }
-                .sheet(isPresented: $isImagePickerDisplay) {
-                    ImagePickerView(selectedImage: $selectedImage[1], sourceType: self.sourceType)
-                }
             }
             
+            
+            VStack {
+                
+            }
+            
+            
         }
-        .padding()
-        .background(Color.green)
-        .border(Color.white, width: 10)
-        .cornerRadius(20)
-        .padding(50)
+        .sheet(isPresented: $isImagePickerDisplay) {
+            ImagePickerView(selectedImage: $selectedImage, sourceType: .camera)
+            ImagePickerView(selectedImage: $selectedImage, sourceType: .photoLibrary)
+        }
     }
 }
-
+    
 struct GridLayoutView_Previews: PreviewProvider {
     static var previews: some View {
-        GridLayoutView(selectedImage: [Image(uiImage: Constant.imagePicture)])
+        GridLayoutView()
     }
 }
